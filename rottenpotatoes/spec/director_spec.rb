7703e3fd9761d7f5e0bc20d9eecd49da9c0c520e
Zoe Wolter <wolterzo@grinnell.edu>
2015-11-02 02:02:37 +0000

    Added RSpec test for Movie directors (in model)

--- rottenpotatoes/spec/director_spec.rb
+++ rottenpotatoes/spec/director_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
+require 'rails_helper'
 
-describe Movies do
-	
+RSpec.describe Movie, :type => :model do
+	it "associates movies with director name" do
+		star_wars_e7 = Movie.create!(title: "Star Wars Episode VII: The Force Awakens",
+																	rating: "PG-13",
+																	release_date: "December 18 2015",
+																	director: "JJ Abrams")
+		star_trek_reboot = Movie.create!(title: "Star Trek",
+																			rating: "PG-13",
+																			release_date: "May 8 2009",
+																			director: "JJ Abrams")
+		star_trek_reboot_2 = Movie.create!(title: "Star Trek: Into Darkness",
+																				rating: "PG-13",
+																				release_date: "May 16 2013",
+																				director: "JJ Abrams")
+		expect(Movie.other_films_with_director (star_wars_e7)).to eq([star_wars_e7,
+																																	star_trek_reboot,
+																																	star_trek_reboot_2])
+	end
 end
